0b52f1822c430165a8c17cbdd9bebed381e7013a
Kiran Jonnalagadda <kiran.jonnalagadda2@cvshealth.com>
2023-04-26 14:41:14 -0400

    removed dependencies

--- src/main/java/com/jk/azure/demo/JkSpringAzureDemoApplication.java
+++ src/main/java/com/jk/azure/demo/JkSpringAzureDemoApplication.java
@@ -2,6 +2,11 @@ package com.jk.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
+=======
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+>>>>>>> 88a20c3 (second commit)
 
 @SpringBootApplication
 @RestController
