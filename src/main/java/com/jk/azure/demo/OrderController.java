b85849cb3d91a6b3c321e67615981de76091305a
Kiran Jonnalagadda <kiran.jonnalagadda2@cvshealth.com>
2023-04-27 17:41:37 -0400

    updated uri

--- src/main/java/com/jk/azure/demo/OrderController.java
+++ src/main/java/com/jk/azure/demo/OrderController.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 public class OrderController {
     private final Map<String, String> orders = new HashMap<>();
 
